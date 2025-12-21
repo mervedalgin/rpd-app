da387671def77e76cb0ec616865255405c9b4a81
Merve DALĞIN <md121012@mail2.gantep.edu.tr>
2025-12-21 04:29:09 +0300

    Fix: Remove duplicate Toaster from page.tsx (layout.tsx already has global Toaster)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import RPDYonlendirme from "@/components/RPDYonlendirme";
 import ConfigurationStatus from "@/components/ConfigurationStatus";
-import { Toaster } from "@/components/ui/sonner";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
       <div className="mt-8 pb-24 lg:pb-4 hidden lg:block">
         <ConfigurationStatus />
       </div>
-      <Toaster />
     </>
   );
 }
